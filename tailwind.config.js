8f1ddb43d63089992fccd5c9214ba48bb89a7cea
yukito <iyggf66974@yahoo.ne.jp>
2025-02-05 09:42:00 +0900

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    backgroundImage: {
+      'image-bg': "url('../src/images/main-bg.jpg')",
+    }
   },
   plugins: [],
 }
